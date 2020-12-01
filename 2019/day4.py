0bd25d1dbe08796dab2ae9f59850dccb7e02c049
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-12-01 16:11:30 -0700

    day 4 solution 2

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,17 +1,30 @@
 def is_valid_password(password):
-    has_double = False
+    counts = [0, ] * 10
+    has_double = 0
     previous = " "
+    count = 0
     for c in str(password):
         if c < previous:
             return False
         if c == previous:
-            has_double = True
+            count += 1
+        elif previous != " ":
+            counts[ord(previous) - ord("0")] = count
+            count = 0
         previous = c
-    return has_double
+    counts[ord(previous) - ord("0")] = count
+
+    try:
+        return min(filter(lambda x: x > 0, counts)) == 1
+    except ValueError:
+        return False
 
 assert(is_valid_password(122345))
-assert(is_valid_password(111123))
-assert(is_valid_password(111111))
+#assert(is_valid_password(111123)) # No longer valid
+#assert(is_valid_password(111111))
+assert(is_valid_password(112233))
+assert(is_valid_password(111122))
+assert(not is_valid_password(123444))
 assert(not is_valid_password(223450))
 assert(not is_valid_password(123789))
 
