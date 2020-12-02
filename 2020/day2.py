b32d377472c35891798fc7318140fdf2efb7c7a0
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-12-01 23:00:21 -0700

    day2 part2

--- 2020/day2.py
+++ 2020/day2.py
@@ -6,7 +6,7 @@ def policy_requirements(policy):
     _min, _max = (int(x) for x in limits.split("-"))
     return character, _min, _max
 
-def validate_password(password):
+def validate_sled_password(password):
     policy, password = password.split(": ")
     characters = [0,] * 26
     for c in password:
@@ -18,14 +18,28 @@ def validate_password(password):
         return False
     return True
 
-assert(validate_password("1-3 a: abcde") == True)
-assert(validate_password("1-3 b: cdefg") == False)
-assert(validate_password("2-9 c: ccccccccc") == True)
+assert(validate_sled_password("1-3 a: abcde") == True)
+assert(validate_sled_password("1-3 b: cdefg") == False)
+assert(validate_sled_password("2-9 c: ccccccccc") == True)
+
+def validate_toboggan_password(password):
+    policy, password = password.split(": ")
+    letter, first, second = policy_requirements(policy)
+
+    if ((password[first - 1] == letter and not password[second - 1] == letter) or
+        (not password[first - 1] == letter and password[second - 1] == letter )):
+        return True
+    return False
+
 
 count = 0
+toboggan = 0
 with open(os.path.join(os.path.dirname(__file__), "day2.txt"), "r") as fp:
     for line in fp:
-        if validate_password(line.strip()):
+        if validate_sled_password(line.strip()):
             count += 1
 
-print(count)
+        if validate_toboggan_password(line):
+            toboggan += 1
+
+print(f"Sled: {count}\nToboggan: {toboggan}")
