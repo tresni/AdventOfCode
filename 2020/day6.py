1e4eb7f114ab7220340d383abd8abedf339bfcef
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-12-06 22:12:38 -0700

    2020 day 6 part 2

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,12 +1,15 @@
 import os.path
+import string
 
 count = 0
 with open(os.path.join(os.path.dirname(__file__), "day6.txt")) as fp:
-    group = set()
+    group = set(string.ascii_lowercase)
     for line in fp:
         if line == "\n":
             count += len(group)
-            group = set()
-        [group.add(c) for c in line.strip()]
+            group = set(string.ascii_lowercase)
+        else:
+            group &= set([c for c in line.strip()])
+
     count += len(group)
 print(count)
