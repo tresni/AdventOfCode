6c09dfaf7cd7748f3748de317b6cb0bb63e1eb98
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2021-04-30 12:56:36 -0600

    day 11 part 2

--- 2020/day11.py
+++ 2020/day11.py
@@ -2,6 +2,7 @@ import os.path
 
 OCCUPIED = "#"
 EMPTY = "L"
+FLOOR = "."
 
 with open(
     os.path.join(
@@ -11,6 +12,34 @@ with open(
     "r",
 ) as fp:
     data = [line.strip() for line in fp]
+
+
+def count_closest(seatmap, row, column):
+    # function takes a "step" tuple of how much to move (vertical, horizontal)
+    #   loop function range(-1, 2) for both axis
+    # stop when either we have "not floor" or index out of bounds
+    # total occupied
+    def step(x, y):
+        srow = row
+        scolumn = column
+        while True:
+            srow += x
+            scolumn += y
+            if srow < 0 or scolumn < 0 or srow == len(seatmap) or scolumn == len(seatmap[row]):
+                return 0
+            elif seatmap[srow][scolumn] == FLOOR:
+                continue
+            elif seatmap[srow][scolumn] == OCCUPIED:
+                return 1
+            else:
+                return 0
+    count = 0
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            count += step(x, y)
+    return count
 
 
 def count_neighbors(seatmap, row, column):
@@ -24,6 +53,7 @@ def count_neighbors(seatmap, row, column):
             except IndexError:
                 pass
         return count
+
     count = 0
     if row > 0:
         count += iterrow(row - 1, column)
@@ -36,18 +66,18 @@ def count_neighbors(seatmap, row, column):
     return count
 
 
-def cycle_seats(seatmap):
+def cycle_seats(seatmap, neighbor_func, to_vacate):
     new_state = []
     for row in range(len(seatmap)):
         new_state.append("")
         for seat in range(len(seatmap[row])):
-            if seatmap[row][seat] == ".":
-                new_state[row] += "."
+            if seatmap[row][seat] == FLOOR:
+                new_state[row] += FLOOR
                 continue
-            neighbors = count_neighbors(seatmap, row, seat)
+            neighbors = neighbor_func(seatmap, row, seat)
             if neighbors == 0:
                 new_state[row] += OCCUPIED
-            elif neighbors >= 4:
+            elif neighbors >= to_vacate:
                 new_state[row] += EMPTY
             else:
                 new_state[row] += seatmap[row][seat]
@@ -67,21 +97,19 @@ def print_seatmap(seatmap):
         print("".join(row))
 
 
-def count_seats(seatmap):
+def count_seats(seatmap, neighbor_func, to_vacate=4):
     current = seatmap
     while True:
-        temp = cycle_seats(current)
+        temp = cycle_seats(current, neighbor_func, to_vacate)
         if compare_seatmaps(temp, current):
             break
-        #print_seatmap(temp)
-        #print()
+        print_seatmap(temp)
+        print()
         current = temp
 
     count = 0
     for line in current:
-        for char in line:
-            if char == OCCUPIED:
-                count += 1
+        count += line.count("#")
     return count
 
 
@@ -98,5 +126,8 @@ test_map = [
     "L.LLLLL.LL",
 ]
 
-assert count_seats(test_map) == 37
-print(count_seats(data))
+assert count_seats(test_map, count_neighbors) == 37
+assert count_seats(data, count_neighbors) == 2113
+
+assert count_seats(test_map, count_closest, 5) == 26
+print(count_seats(data, count_closest, 5))
