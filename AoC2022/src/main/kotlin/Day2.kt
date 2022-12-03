4dfcba7c667b8664cdf7c2c7eee5d776497be610
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-02 23:53:05 -0700

    Day 2

---
+++ AoC2022/src/main/kotlin/Day2.kt
@@ -0,0 +1,70 @@
+import java.io.File
+
+class Day2(val input: String) {
+    fun score(): Int {
+        return input.uppercase().split("\n")
+            .map {
+                val plays = it.split(" ", limit = 2)
+                shapeTable[plays[1]]?.let { shape ->
+                    return@map shape.points +
+                        if (shape.weak.primary == plays[0]) {
+                            LOSE
+                        } else if (shape.opp.primary == plays[0]) {
+                            TIE
+                        } else {
+                            WIN
+                        }
+                } ?: 0
+            }
+            .sum()
+    }
+
+    fun rig(): Int {
+        return input.uppercase().split("\n")
+            .map {
+                val plays = it.split(" ", limit = 2)
+                shapeTable[plays[0]]?.let { opp ->
+                    return@map when (plays[1]) {
+                        "X" -> LOSE + (shapeTable[opp.strong.secondary]?.points ?: 0)
+                        "Y" -> TIE + (shapeTable[opp.opp.secondary]?.points ?: 0)
+                        else -> WIN + (shapeTable[opp.weak.secondary]?.points ?: 0)
+                    }
+                } ?: 0
+            }
+            .sum()
+    }
+
+    companion object {
+        enum class RPS(val primary: String, val secondary: String) {
+            ROCK("A", "X"),
+            PAPER("B", "Y"),
+            SCISSORS("C", "Z"),
+        }
+
+        data class HandShape(val opp: RPS, val weak: RPS, val strong: RPS, val points: Int)
+
+        val ROCK = HandShape(RPS.ROCK, RPS.PAPER, RPS.SCISSORS, 1)
+        val PAPER = HandShape(RPS.PAPER, RPS.SCISSORS, RPS.ROCK, 2)
+        val SCISSORS = HandShape(RPS.SCISSORS, RPS.ROCK, RPS.PAPER, 3)
+
+        const val WIN = 6
+        const val TIE = 3
+        const val LOSE = 0
+
+        val shapeTable = mapOf(
+            RPS.ROCK.primary to ROCK,
+            RPS.ROCK.secondary to ROCK,
+            RPS.PAPER.primary to PAPER,
+            RPS.PAPER.secondary to PAPER,
+            RPS.SCISSORS.primary to SCISSORS,
+            RPS.SCISSORS.secondary to SCISSORS
+        )
+    }
+}
+
+fun main() {
+    val input = File("src/main/resources/Day2/input.txt").readText()
+
+    println(Day2(input).score())
+    println(Day2(input).rig())
+}
---
+++ AoC2022/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,61 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
+import java.util.stream.Stream
+
+class Day2Test {
+
+    @Test
+    fun `basic tests`() {
+        Day2("A Y").score() shouldBe 8
+        Day2("B X").score() shouldBe 1
+        Day2("C Z").score() shouldBe 6
+    }
+
+    @Test
+    fun `should end with a score of 15`() {
+        val input = File("src/test/resources/Day2/input.txt").readText()
+        Day2(input).score() shouldBe 15
+    }
+
+    @ParameterizedTest
+    @MethodSource("handShapeStream")
+    fun `ensure scores`(handShape: Day2.Companion.HandShape) {
+        Day2("${handShape.weak.primary} ${handShape.opp.secondary}").score() shouldBe Day2.LOSE + handShape.points
+        Day2("${handShape.opp.primary} ${handShape.opp.secondary}").score() shouldBe Day2.TIE + handShape.points
+        Day2("${handShape.strong.primary} ${handShape.opp.secondary}").score() shouldBe Day2.WIN + handShape.points
+    }
+
+    @Test
+    fun `basic rigged`() {
+        Day2("A Y").rig() shouldBe 4
+        Day2("B X").rig() shouldBe 1
+        Day2("C Z").rig() shouldBe 7
+    }
+
+    @Test
+    fun `extended rigged`() {
+        val input = File("src/test/resources/Day2/input.txt").readText()
+        Day2(input).rig() shouldBe 12
+    }
+
+    @ParameterizedTest
+    @MethodSource("handShapeStream")
+    fun `ensure rigged`(handShape: Day2.Companion.HandShape) {
+        Day2("${handShape.opp.primary} X").rig() shouldBe Day2.LOSE + Day2.shapeTable[handShape.strong.primary]!!.points
+        Day2("${handShape.opp.primary} Y").rig() shouldBe Day2.TIE + handShape.points
+        Day2("${handShape.opp.primary} Z").rig() shouldBe Day2.WIN + Day2.shapeTable[handShape.weak.primary]!!.points
+    }
+
+    companion object {
+        @JvmStatic
+        fun handShapeStream(): Stream<Arguments> = Stream.of(
+            Arguments.of(Day2.ROCK),
+            Arguments.of(Day2.PAPER),
+            Arguments.of(Day2.SCISSORS)
+        )
+    }
+}
