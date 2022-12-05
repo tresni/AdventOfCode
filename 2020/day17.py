db94655a3116ac60d0f400d5c5f1905a60cc3375
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-04 21:01:43 -0700

    2020 Day 17, part 2

--- 2020/day17.py
+++ 2020/day17.py
@@ -10,59 +10,57 @@ class Coord(object):
     X = 0
     Y = 1
     Z = 2
+    W = 3
 
 class PocketDimension(object):
-    __initSize: tuple[int, int, int] = [0, 0, 0]
+    __initSize: tuple[int, int, int, int] = [0, 0, 0, 0]
     __cycle: int = 0
-    __state: dict[tuple[int, int, int],  str] = defaultdict(lambda: INACTIVE)
+    __state: dict[tuple[int, int, int, int],  str] = defaultdict(lambda: INACTIVE)
 
     def __init__(self, data: str):
         yIndex: int = 0
         for line in data.splitlines(keepends=False):
             lineLength = len(line)
             self.__initSize[Coord.X] = max(self.__initSize[Coord.X], lineLength)
-            if not line:
-                self.__initSize[Coord.Z] += 1
-                self.__initSize[Coord.Y] = max(self.__initSize[Coord.Y], yIndex)
-                yIndex = 0
             for index in range(lineLength):
-                self.__state[index, yIndex, self.__initSize[Coord.Z]] = line[index]
+                self.__state[index, yIndex, 0, 0] = line[index]
             yIndex += 1
         self.__initSize[Coord.Y] = max(self.__initSize[Coord.Y], yIndex)
 
-    def __active_neighbor_count(self, coordinates: tuple[int, int, int]) -> int:
+    def __active_neighbor_count(self, coordinates: tuple[int, int, int, int]) -> int:
         count = 0
         offsets = [-1, 0 , 1]
 
-        for zOffset in offsets:
+        for xOffset in offsets:
             for yOffset in offsets:
-                for xOffset in offsets:
-                    if not zOffset and not yOffset and not xOffset: continue
-                    if self.__state[(
-                        coordinates[Coord.X] + xOffset,
-                        coordinates[Coord.Y] + yOffset,
-                        coordinates[Coord.Z] + zOffset,
-                    )] == ACTIVE:
-                        count += 1
+                for zOffset in offsets:
+                    for wOffset in offsets:
+                        if not zOffset and not yOffset and not xOffset and not wOffset: continue
+                        if self.__state[(
+                            coordinates[Coord.X] + xOffset,
+                            coordinates[Coord.Y] + yOffset,
+                            coordinates[Coord.Z] + zOffset,
+                            coordinates[Coord.W] + wOffset
+                        )] == ACTIVE:
+                            count += 1
         return count
 
 
     def cycle(self):
         self.__cycle += 1
-        newState: dict[tuple[int, int, int], str] = defaultdict(lambda: INACTIVE)
+        newState: dict[tuple[int, int, int, int], str] = defaultdict(lambda: INACTIVE)
         cycle = self.__cycle
-        for x in range(-cycle, cycle + self.__initSize[Coord.X]):
-            for y in range(-cycle, cycle + self.__initSize[Coord.Y]):
-                for z in range(-cycle, cycle + self.__initSize[Coord.Z] + 1):
-                    cell = (x, y, z)
-                    active = self.__active_neighbor_count(cell)
-                    currentState = self.__state[cell]
-                    if currentState == INACTIVE and active == 3:
-                        newState[cell] = ACTIVE
-                    elif currentState == ACTIVE and active not in [2, 3]:
-                        newState[cell] = INACTIVE
-                    else:
-                        newState[cell] = currentState
+        for w in range(-cycle, cycle + 1):
+            for z in range(-cycle, cycle + 1):
+                for y in range(-cycle, cycle + self.__initSize[Coord.Y]):
+                    for x in range(-cycle, cycle + self.__initSize[Coord.X]):
+                        cell = (x, y, z, w)
+                        active = self.__active_neighbor_count(cell)
+                        currentState = self.__state[cell]
+                        if currentState == INACTIVE and active == 3:
+                            newState[cell] = ACTIVE
+                        elif currentState == ACTIVE and active in [2, 3]:
+                            newState[cell] = ACTIVE
         self.__state = newState
 
     def countActive(self, ) -> int:
@@ -70,11 +68,13 @@ class PocketDimension(object):
 
     def printState(self):
         cycle = self.__cycle
-        for z in range(-cycle, cycle + self.__initSize[Coord.Z] + 1):
-            print(f"z={z}")
-            for y in range(-cycle, cycle + self.__initSize[Coord.Y]):
-                for x in range(-cycle, cycle + self.__initSize[Coord.X]):
-                    print(self.__state[(x, y, z)], end="")
+        for w in range(-cycle, cycle + 1):
+            for z in range(-cycle, cycle + self.__initSize[Coord.Z] + 1):
+                print(f"z={z}, w={w}")
+                for y in range(-cycle, cycle + self.__initSize[Coord.Y]):
+                    for x in range(-cycle, cycle + self.__initSize[Coord.X]):
+                        print(self.__state[(x, y, z, w)], end="")
+                    print()
                 print()
             print()
 
@@ -90,7 +90,7 @@ test.printState()
 for i in range(6):
     test.cycle()
 
-assert test.countActive() == 112
+assert test.countActive() == 848
 
 full = """
 .#######
