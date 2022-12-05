4b667730a180c8c3b57c9113d09a76b407b65b01
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-04 23:29:06 -0700

    Day 5, Part 1

---
+++ AoC2022/src/main/kotlin/Day5.kt
@@ -0,0 +1,43 @@
+class Day5(input: String) {
+
+    internal val crates: List<MutableList<String>>
+    internal val instructions: List<String>
+
+    init {
+        val (initial, details) = input.split("\n\n")
+        crates = Crates(initial)
+        instructions = details.split("\n")
+    }
+
+    fun runInstructions(): String {
+        instructions.forEach {
+            "move (\\d+) from (\\d+) to (\\d+)".toRegex().find(it)?.let { match ->
+                val count = match.groupValues[1].toInt()
+                val source = match.groupValues[2].toInt() - 1
+
+                this.crates[match.groupValues[3].toInt() - 1].addAll(this.crates[source].takeLast(count).reversed())
+                repeat(count) { this.crates[source].removeAt(this.crates[source].size - 1) }
+            }
+        }
+        return crates.joinToString("") { it.last() }
+    }
+    companion object {
+        internal fun Crates(input: String): List<MutableList<String>> {
+            val crates = mutableMapOf<Int, MutableList<String>>()
+            input.split("\n").map {
+                if (!it.contains("[")) return@map
+                it.chunked(4).forEachIndexed { index, s ->
+                    if (s.isNotBlank()) {
+                        crates.getOrPut(index) { mutableListOf() }.add(0, s[1].toString())
+                    }
+                }
+            }
+            return crates.toSortedMap().map { it.value }
+        }
+    }
+}
+
+fun main() {
+    val input = readInput(5)
+    println(Day5(input).runInstructions())
+}
---
+++ AoC2022/src/test/kotlin/Day5Test.kt
@@ -0,0 +1,26 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day5Test: BaseDayTest(5) {
+    @Test
+    fun `read initial crates properly`() {
+        Day5.Crates(
+            """
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+             1   2   3
+        """.trimIndent()
+        ) shouldBe listOf(listOf("Z", "N"), listOf("M", "C", "D"), listOf("P"))
+    }
+
+    @Test
+    fun `Reads crates from input`() {
+        Day5(input).crates shouldBe listOf(listOf("Z", "N"), listOf("M", "C", "D"), listOf("P"))
+    }
+
+    @Test
+    fun `follows instructions`() {
+        Day5(input).runInstructions() shouldBe "CMZ"
+    }
+}
