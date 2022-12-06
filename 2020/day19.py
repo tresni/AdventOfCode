2dd3801fa09ae82958d43a51e66df4b8b8c52003
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-05 21:45:23 -0700

    2020 Day 19, Part 1

---
+++ 2020/day19.py
@@ -0,0 +1,84 @@
+import os
+import re
+
+class RulesParser(object):
+    _rules: dict[int, str]
+    _parsed: dict[int, str]
+    _regex: re.Pattern
+
+    def __init__(self, rules: str):
+        self._parsed = {}
+        self._rules = {}
+
+        for line in rules.split("\n"):
+            index, rule = line.split(":")
+            self._rules[int(index)] = rule.split()
+        
+        self._parsed[0] = self.create(self._rules[0])
+        self._regex = re.compile(f"^{self._parsed[0]}$")
+
+    def create(self, rule: list[str]) -> str:
+        parsed = "(?:"
+        for x in rule:
+            if x.isnumeric():
+                x = int(x)
+                if x not in self._parsed:
+                    self._parsed[x] = self.create(self._rules[x])
+                parsed += self._parsed[x]
+            else:
+                parsed += x.strip("\"")
+        parsed += ")"
+        return parsed
+                
+    def regex(self) -> str:
+        return self._parsed[0]
+
+    def match(self, target: str) -> bool:
+        return self._regex.match(target) != None
+
+rp = RulesParser("""
+0: 1 2
+1: "a"
+2: 1 3 | 3 1
+3: "b"
+""".strip())
+
+assert rp.regex() == r"(?:(?:a)(?:(?:a)(?:b)|(?:b)(?:a)))"
+
+assert rp.match("aab")
+assert rp.match("aba")
+assert not rp.match("baa")
+assert not rp.match("abaa")
+
+rp = RulesParser("""
+0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+""".strip())
+
+assert rp.match("aaaabb")
+assert rp.match("aaabab")
+assert rp.match("abbabb")
+assert rp.match("abbbab")
+assert rp.match("aabaab")
+assert rp.match("aabbbb")
+assert rp.match("abaaab")
+assert rp.match("ababbb")
+
+assert not rp.match("bababa")
+assert not rp.match("aaabbb")
+assert not rp.match("aaaabbb")
+
+with open(
+    os.path.join(
+        os.path.dirname(__file__),
+        os.path.splitext(os.path.basename(__file__))[0] + ".txt",
+    ),
+    "r",
+) as fp:
+    rules, messages = fp.read().split("\n\n")
+    rp = RulesParser(rules)
+    print(sum([1 if rp.match(x) else 0 for x in messages.split()]))
