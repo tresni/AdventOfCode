faada0f48ac6264012bd6ba4db4331518bb58a61
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-06 23:08:05 -0700

    Missed Day6 tests :(

---
+++ AoC2022/src/test/kotlin/Day6Test.kt
@@ -0,0 +1,14 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day6Test : BaseDayTest(5) {
+
+    @Test
+    fun `initial tests`() {
+        Day6.startOfInput("mjqjpqmgbljsphdztnvjfqwrcgsmlb") shouldBe  7
+        Day6.startOfInput("bvwbjplbgvbhsrlpgdmjqwftvncz") shouldBe 5
+        Day6.startOfInput("nppdvjthqldpwncqszvftbrmjlhg") shouldBe 6
+        Day6.startOfInput("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") shouldBe 10
+        Day6.startOfInput("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") shouldBe 11
+    }
+}
