e9d03777325b8e0dc426e06785f36626ee93d517
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-08 01:29:22 -0700

    Day 8, Parts 1 & 2, over-optimized

---
+++ AoC2022/src/main/kotlin/Day8.kt
@@ -0,0 +1,47 @@
+class Day8(input: String) {
+    val trees: MutableList<List<Int>> = mutableListOf()
+
+    init {
+        input.lines().forEach {
+            trees.add(it.map { c -> c.toString().toInt() })
+        }
+    }
+
+    fun isVisible(x: Int, y: Int): Boolean {
+        if ( y == 0 || y == trees.count() -1 || x == 0 || x == trees[y].count() - 1) return true
+        return trees[y].splitsExceedValueAtIndex(x) || trees.map { it[x] }.splitsExceedValueAtIndex(y)
+    }
+
+    fun scenicScore(x: Int, y: Int): Int {
+        if ( y == 0 || y == trees.count() -1 || x == 0 || x == trees[y].count() - 1) return 0
+        val target = trees[y][x]
+        return trees[y].splitAndTarget(x, target) * trees.map { it[x] }.splitAndTarget(y, target)
+    }
+
+    fun countVisible() = trees.mapIndexed { y, it -> it.filterIndexed { x, _ -> isVisible(x, y) }.count() }.sum()
+    fun mostScenic() = trees.mapIndexed { y, row -> List(row.size) { x -> scenicScore(x, y) }.max() }.max()
+
+    companion object {
+        private fun List<Int>.split(index: Int): Pair<List<Int>, List<Int>> =
+            Pair(this.subList(0, index), this.subList(index + 1, this.count()))
+
+        fun List<Int>.splitsExceedValueAtIndex(index: Int): Boolean =
+            this.split(index).let { (left, right) -> this[index] > left.max() || this[index] > right.max() }
+
+        fun List<Int>.splitAndTarget(index: Int, target: Int): Int =
+            this.split(index).let { (left, right) ->
+                left.reversed().filterToTarget(target) * right.filterToTarget(target)
+            }
+        private fun List<Int>.filterToTarget(target: Int): Int =
+            if (this.isNotEmpty()) {
+                this.takeWhile { it < target }.count().let {
+                    if (this.size > it) it + 1 else it
+                }
+            } else 0
+    }
+}
+
+fun main() {
+    println(Day8(readInput(8)).countVisible())
+    println(Day8(readInput(8)).mostScenic())
+}
---
+++ AoC2022/src/test/kotlin/Day8Test.kt
@@ -0,0 +1,49 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day8Test : BaseDayTest(8) {
+    @Test
+    fun test() {
+        Day8("222\n212\n222").isVisible(1,1) shouldBe false
+        Day8("222\n212\n222").isVisible(0, 0) shouldBe true
+        Day8("222\n212\n222").isVisible(2,2) shouldBe true
+    }
+
+    @Test
+    fun withInput() {
+        Day8(input).also {
+            it.isVisible(0, 0) shouldBe true
+            it.isVisible(4, 0) shouldBe true
+            it.isVisible(0, 4) shouldBe true
+            it.isVisible(4 ,4) shouldBe true
+
+
+            it.isVisible(0, 3) shouldBe true
+            it.isVisible(1, 2) shouldBe true
+            it.isVisible(1, 3) shouldBe false
+            it.isVisible(1, 4) shouldBe true
+            it.isVisible(2, 3) shouldBe true
+
+            it.isVisible(1, 4) shouldBe true
+            it.isVisible(2, 2) shouldBe false
+            it.isVisible(3, 1) shouldBe false
+            it.isVisible(3, 3) shouldBe false
+            it.countVisible() shouldBe 21
+        }
+    }
+
+    @Test
+    fun scenicScore() {
+        Day8("111\n121\n111").also {
+            it.scenicScore(0,0) shouldBe 0
+            it.scenicScore(0,2) shouldBe 0
+            it.scenicScore(2, 0) shouldBe 0
+            it.scenicScore(2, 2) shouldBe 0
+            it.scenicScore(1,1) shouldBe 1
+        }
+        Day8(input).also {
+            it.scenicScore(2, 1) shouldBe 4
+            it.scenicScore(2, 3) shouldBe 8
+        }
+    }
+}
