382765a098fb956c7a9d8640a662a0c3ae130ead
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-12-08 21:28:13 -0700

    More cleanup

--- AoC2022/src/main/kotlin/Day8.kt
+++ AoC2022/src/main/kotlin/Day8.kt
@@ -1,5 +1,5 @@
 class Day8(input: String) {
-    val trees: MutableList<List<Int>> = mutableListOf()
+    private val trees: MutableList<List<Int>> = mutableListOf()
 
     init {
         input.lines().forEach {
@@ -8,22 +8,22 @@ class Day8(input: String) {
     }
 
     fun isVisible(x: Int, y: Int): Boolean {
-        if (y == 0 || y == trees.count() - 1 || x == 0 || x == trees[y].count() - 1) return true
+        if (y == 0 || y == trees.size - 1 || x == 0 || x == trees[y].size - 1) return true
         return trees[y].splitsExceedValueAtIndex(x) || trees.map { it[x] }.splitsExceedValueAtIndex(y)
     }
 
     fun scenicScore(x: Int, y: Int): Int {
-        if (y == 0 || y == trees.count() - 1 || x == 0 || x == trees[y].count() - 1) return 0
+        if (y == 0 || y == trees.size - 1 || x == 0 || x == trees[y].size - 1) return 0
         val target = trees[y][x]
         return trees[y].splitAndTarget(x, target) * trees.map { it[x] }.splitAndTarget(y, target)
     }
 
-    fun countVisible() = trees.mapIndexed { y, it -> it.filterIndexed { x, _ -> isVisible(x, y) }.count() }.sum()
+    fun countVisible() = trees.mapIndexed { y, it -> it.filterIndexed { x, _ -> isVisible(x, y) }.size }.sum()
     fun mostScenic() = trees.mapIndexed { y, row -> List(row.size) { x -> scenicScore(x, y) }.max() }.max()
 
     companion object {
         private fun List<Int>.split(index: Int): Pair<List<Int>, List<Int>> =
-            Pair(this.subList(0, index), this.subList(index + 1, this.count()))
+            Pair(this.subList(0, index), this.subList(index + 1, this.size))
 
         fun List<Int>.splitsExceedValueAtIndex(index: Int): Boolean =
             this.split(index).let { (left, right) -> this[index] > left.max() || this[index] > right.max() }
@@ -35,7 +35,7 @@ class Day8(input: String) {
 
         private fun List<Int>.filterToTarget(target: Int): Int =
             if (this.isNotEmpty()) {
-                this.takeWhile { it < target }.count().let {
+                this.takeWhile { it < target }.size.let {
                     if (this.size > it) it + 1 else it
                 }
             } else 0
